63fda9e0c0b98513404778b6b7325fc4e2b6ce56
Hossain <proffesergio@gmail.com>
2024-08-19 10:54:43 +0600

    Temperature Value Changed in Farenheit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 async function getWeatherData() {
-    const URL = "https://api.openweathermap.org/data/2.5/weather?lat=23.80203426659407&lon=90.41696766576916&appid=712af7c3d1de067b43f618336419e646"
+    const URL = "https://api.openweathermap.org/data/2.5/weather?lat=23.80203426659407&lon=90.41696766576916&appid=712af7c3d1de067b43f618336419e646&units=imperial"
 
     const weatherPromise = await fetch(URL)
     const weatherData = await weatherPromise.json()
