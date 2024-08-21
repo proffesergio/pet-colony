cbdae31019932506ed3390271ea0145e8a97cd8e
Hossain <proffesergio@gmail.com>
2024-08-21 16:12:10 +0600

    Pet Filter Area Comes into Life

--- main.js
+++ main.js
@@ -26,13 +26,21 @@ async function dynamicPets() {
 
     const parsedPets = petsData.pets
 
-    console.log(petsData)
-
     parsedPets.forEach((pet) => {
         const clone = template.content.cloneNode(true)
 
+        clone.querySelector(".pet-card").dataset.species = pet.species
+
         clone.querySelector("h3").textContent = pet.name
         clone.querySelector("p").textContent = pet.description
+
+        clone.querySelector(".pet-age").textContent = petAgeCalculator(pet.birthYear)
+
+        if (!pet.photo) pet.photo = "images/fallback.jpg"
+        clone.querySelector(".pet-card-photo img").src = pet.photo
+        clone.querySelector(".pet-card-photo").alt = `A ${pet.species} named ${pet.name}`
+
+
 
         wrapper.appendChild(clone)
     })
@@ -41,7 +49,42 @@ async function dynamicPets() {
 }
 dynamicPets()
 
+function petAgeCalculator(birthYear) {
+    const currentYear = new Date().getFullYear()
 
+    const age = currentYear - birthYear
+    if (age == 1) return "1 year old"
+    if (age < 1) return "Less than a year old"
+
+    return age + " years old"
+}
+
+// pet filter button code
+
+const petFilterButtons = document.querySelectorAll(".pet-filter button")
+
+petFilterButtons.forEach(element => {
+    element.addEventListener("click", handleClickEvent)
+})
+
+function handleClickEvent(event) {
+    // remove active class
+    petFilterButtons.forEach(element => element.classList.remove("active"))
+
+    // add active class to the clicked button
+    event.target.classList.add("active")
+
+    // filter pets
+    const currentFilter = event.target.dataset.filter
+
+    document.querySelectorAll(".pet-card").forEach(el => {
+        if (currentFilter == el.dataset.species || currentFilter == "all") {
+            el.style.display = "grid"
+        } else {
+            el.style.display = "none"
+        }
+    })
+}
 
 // async function getPetData() {
 //     const URL = "https://github.com/LearnWebCode/json-example/blob/master/pets-data.json"
